357403208e05e915cbdf35eddf393a5830fc8626
ray.li <ray.li@shopee.com>
2024-05-08 19:38:39 +0800

    add unweighted single source shortest path

--- graph/src/bfs/USSSPath.java
+++ graph/src/bfs/USSSPath.java
@@ -8,17 +8,23 @@ import java.util.*;
 public class USSSPath {
     private Graph G;
     boolean[] visited;
+    int source;
+
+    int[] pre;
+
+    int[] distance;
 
     private List<Integer> order = new ArrayList<>();
 
-    public USSSPath(Graph g) {
+    public USSSPath(Graph g, int s) {
         G = g;
         visited = new boolean[G.getVertex()];
-
-        for (int i = 0; i < G.getVertex(); i++) {
-            if (!visited[i])
-                bfs(i);
-        }
+        pre = new int[G.getVertex()];
+        Arrays.fill(pre, -1);
+        distance = new int[G.getVertex()];
+        Arrays.fill(distance, -1);
+        source = s;
+        bfs(s);
     }
 
     // time complexity is O(v+e)
@@ -26,6 +32,8 @@ public class USSSPath {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(s);
         visited[s] = true;
+        pre[s] = s;
+        distance[s] = 0;
         while (!queue.isEmpty()) {
             int vertex = queue.poll();
             order.add(vertex);
@@ -34,14 +42,45 @@ public class USSSPath {
                 if (!visited[adj]) {
                     queue.add(adj);
                     visited[adj] = true;
+                    pre[adj] = vertex;
+                    distance[adj] = distance[vertex] + 1;
                 }
             }
         }
     }
 
+    public boolean isConnectedTo(int t) {
+        return visited[t];
+    }
+
+
+    /**
+     * get the shortest dis from f to source
+     * @param f
+     * @return
+     */
+    public int dis(int f) {
+        return distance[f];
+    }
+
+    public List<Integer> path(int t) {
+        List<Integer> res = new ArrayList<>();
+        if (!isConnectedTo(t))
+            return res;
+        int cur = t;
+        while (cur != source) {
+            res.add(cur);
+            cur = pre[cur];
+        }
+        res.add(source);
+        Collections.reverse(res);
+        return res;
+    }
+
     public static void main(String[] args) {
         Graph graph = new Graph("graph3.txt");
-        USSSPath graphBFS = new USSSPath(graph);
-        System.out.println("BFS order " + graphBFS.order);
+        USSSPath usssPath = new USSSPath(graph , 0);
+        System.out.println(usssPath.dis(4));
+        System.out.println(usssPath.dis(5));
     }
 }
