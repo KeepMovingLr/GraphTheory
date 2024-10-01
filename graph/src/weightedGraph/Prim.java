6867ca894f3e89747aa250ebda2097d9bd580e3f
Ray <1040375843@qq.com>
2024-09-30 20:35:50 -0500

    reduce code

--- graph/src/weightedGraph/Prim.java
+++ graph/src/weightedGraph/Prim.java
@@ -38,13 +38,50 @@ public class Prim {
         }
     }
 
+    // another version
+    public Prim(WeightedGraph G, int version) {
+        this.G = G;
+        mst = new ArrayList<>();
+        // check if it is connected
+        CC cc = new CC(G);
+        if (cc.ccCount != 1) return;
+
+        // Prim,  time complexity E * log(E)
+        PriorityQueue<WeightedEdge> pq = new PriorityQueue<>(Comparator.comparingInt(WeightedEdge::getWeight));
+        Set<Integer> partition = new HashSet<>();
+        int check = 0;
+        partition.add(check);
+        for (int nextV : G.getAdj()[0].keySet()) {
+            pq.add(new WeightedEdge(check, nextV, G.getAdj()[0].get(nextV)));
+        }
+        while (!pq.isEmpty()) {
+            WeightedEdge smallest = pq.poll();
+            while (smallest != null && partition.contains(smallest.getV()) && partition.contains(smallest.getW())) {
+                smallest = pq.poll();
+            }
+            if (smallest == null) break;
+            mst.add(smallest);
+
+            // get all cut edges
+            check = partition.contains(smallest.getV()) ? smallest.getW() : smallest.getV();
+            partition.add(check);
+
+            TreeMap<Integer, Integer> adjs = G.getAdjs(check);
+            for (Integer node : adjs.keySet()) {
+                if (!partition.contains(node)) { // i - node
+                    pq.add(new WeightedEdge(check, node, adjs.get(node)));
+                }
+            }
+        }
+    }
+
     public ArrayList<WeightedEdge> result() {
         return mst;
     }
 
     public static void main(String[] args) {
         WeightedGraph weightedGraph = new WeightedGraph("src/weightedGraph/graph.txt");
-        Prim prim = new Prim(weightedGraph);
+        Prim prim = new Prim(weightedGraph, 0);
         System.out.println(prim.result());
     }
 }
